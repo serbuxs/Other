bb9b67dd63ec9165395a567def40af3f6dddbfac
Siarhei Bukhavets <sbukhovets@gmail.com>
2021-03-22 21:58:37 +0300

    Added release files with gui form

---
+++ GUI.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class GUI extends JFrame{
+    private JButton btnGen;
+    private JPanel rootPanel;
+    private JLabel lblKey;
+
+    public GUI(){
+        // for use the designer form
+        add(rootPanel);
+
+        setVisible(true);
+        setTitle("Key Generator");
+        setSize(240,180);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        btnGen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                keyGenerate a = new keyGenerate();
+                try {
+                    a.keyGenerate();
+                    lblKey.setText(a.getStr());
+                    //lblKey.setText("Successfully written!");
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        });
+    }
+}
