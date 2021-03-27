6ad3a0eef5cb72b6a15368f843bbfbdd433880df
Siarhei Bukhavets <sbukhovets@gmail.com>
2021-03-28 00:52:26 +0300

    Added comments.

--- formFiller/src/formFill.java
+++ formFiller/src/formFill.java
@@ -8,22 +8,28 @@ public class formFill {
         System.setProperty("webdriver.chrome.driver","c:\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
+        // base url
         String url = "https://account.habr.com/login";
 
+        // get base url and open in browser
         driver.get(url);
 
+        // find email and password elements by id on the page
         WebElement email = driver.findElement(By.id("email_field"));
         WebElement password = driver.findElement(By.id("password_field"));
 
+        // send values to email and password fields
         email.sendKeys("sbukhovets@gmail.com");
         password.sendKeys("proSerique5");
         System.out.println("TextFields are filled!");
 
+        // find button element and click it
         WebElement login = driver.findElement(By.name("go"));
         login.click();
 
         System.out.println("Login done!");
 
+        // close tab or browser
         driver.close();
     }
 }
